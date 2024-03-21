8ef11772ab597eacdbdbc33de6ee3d5abeea5299
Santiago Quihui <santiagoquihui@hotmail.com>
2024-03-20 22:40:08 -0600

    Initial Commit

---
+++ VisionOSDrawing/VisionOSDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSDrawingApp.swift
+//  VisionOSDrawing
+//
+//  Created by Santiago Quihui on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
